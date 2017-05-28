d9e4a2a0845414a66efb29168129e3a427a48590
Lara Carvalho <laraftoller@gmail.com>
2017-05-28 17:20:44 -0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+//////////////////////////////
+///////// Gulp File /////////
+////////////////////////////
+
+/* 			-- COMMON GULP FUNCTIONS --
+	
+	gulp.task -- Define tasks Gulp should operate.
+	gulp.src -- Direct Gulp to files it should use.
+	gulp.dest -- Folder it outputs.
+	gulp.watch -- Watch files and folders for changes.
+*/
+
+
+///// MODULES /////
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-minify');
+const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+
+///// TASKS /////
+
+// Compiling SCSS to CSS
+
+gulp.task('sass', function() {
+	gulp.src('src/scss/*.scss')
+    	.pipe(sass().on('error', sass.logError))
+    	.pipe(cssnano())
+    	.pipe(gulp.dest('dist/css'));
+});
+
+// Minifying JavaScript
+
+gulp.task('minify', function() {
+	gulp.src('src/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+// Image Minifying and Optimization
+
+gulp.task('imagemin', function() {
+	gulp.src('src/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/imgs'))
+});
+
+// Browser Sync & Live Reload
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+///// WATCH FILES /////
+ 
+gulp.task('watch', function() {
+	gulp.watch('src/scss/*.scss', ['sass']);
+	gulp.watch('src/js/*.js', ['minify']);
+	gulp.watch('src/images/*', ['imagemin']);
+});
+
+///// DEFAULT /////
+
+gulp.task('default', ['browserSync', 'styles']);
